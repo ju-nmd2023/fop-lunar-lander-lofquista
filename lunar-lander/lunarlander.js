23f8094752bdac655736aefa15a5173879aa6d89
lofquista <loal23gk@student.ju.se>
2024-02-01 14:06:50 +0100

    change of files

--- lunar-lander/lunarlander.js
+++ lunar-lander/lunarlander.js
@@ -10,19 +10,28 @@ function scenery() {
   fill("green");
   rect(0, 230, width, 100);
 
-  let sunX = 465;
-  let sunY = 45;
+  // sun
+  let sunX = 570;
+  let sunY = 25;
 
   fill(249, 215, 28);
   ellipse(sunX, sunY, 60, 60);
   stroke(249, 215, 28);
-  line(435, 25, 400, 10, 5, 10);
-  line(430, 40, 400, 38, 5, 10);
-  line(430, 55, 400, 60, 5, 10);
-  line(435, 70, 405, 85, 5, 10);
-  line(445, 80, 435, 100, 5, 10);
-  line(445, 80, 435, 100, 5, 10);
+  line(sunX - 30, sunY - 20, sunX - 65, sunY - 30, 5, 10);
+  line(sunX - 35, sunY, sunX - 65, sunY - 2, 5, 10);
+  line(sunX - 35, sunY + 15, sunX - 65, sunY + 20, 5, 10);
+  line(sunX - 30, sunY + 28, sunX - 60, sunY + 45, 5, 10);
+  line(sunX - 20, sunY + 35, sunX - 30, sunY + 60, 5, 10);
+  line(sunX - 5, sunY + 40, sunX - 5, sunY + 65, 5, 10);
+  line(sunX + 10, sunY + 38, sunX + 15, sunY + 65, 5, 10);
+  line(sunX + 25, sunY + 35, sunX + 40, sunY + 55, 5, 10);
 
+  pop();
+
+  push();
+  noStroke();
+  fill(83, 53, 10);
+  rect(350, 100, 25, 150);
   pop();
 }
 
