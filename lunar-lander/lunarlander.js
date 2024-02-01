d6e3b4d848b35384bac34408b1f9f7d2bfae865d
lofquista <loal23gk@student.ju.se>
2024-02-01 14:44:09 +0100

    tree branch

--- lunar-lander/lunarlander.js
+++ lunar-lander/lunarlander.js
@@ -5,17 +5,21 @@ function setup() {
 
 function scenery() {
   push();
+
   noStroke();
   // grass
   fill("green");
   rect(0, 230, width, 100);
 
-  // sun
+  // SUN
   let sunX = 570;
   let sunY = 25;
 
+  // sun
   fill(249, 215, 28);
   ellipse(sunX, sunY, 60, 60);
+
+  // sun rays
   stroke(249, 215, 28);
   line(sunX - 30, sunY - 20, sunX - 65, sunY - 30, 5, 10);
   line(sunX - 35, sunY, sunX - 65, sunY - 2, 5, 10);
@@ -28,13 +32,47 @@ function scenery() {
 
   pop();
 
+  // TREE
+  // tree trunk
   push();
   noStroke();
   fill(83, 53, 10);
-  rect(350, 100, 25, 150);
+  rect(350, 100, 20, 150);
+
+  // tree branch
+  push();
+  stroke(83, 53, 10);
+  strokeWeight(4);
+  line(360, 165, 300, 150);
+  pop();
+
+  // tree crown
+  fill(42, 126, 25);
+  ellipse(335, 100, 40, 40);
+  ellipse(365, 110, 50, 40);
+
+  push();
+  fill(32, 116, 15);
+  ellipse(388, 108, 40, 35);
+  pop();
+
+  ellipse(385, 90, 40, 40);
+
+  push();
+  fill(32, 116, 15);
+  ellipse(360, 75, 45, 45);
+  ellipse(342, 115, 25, 30);
+  pop();
+
   pop();
 }
 
 function draw() {
   scenery();
 }
+
+// TO DO:
+// add a branch on the branch
+// make tree in variables
+// check canvas size on smaller computer
+// make tree taller
